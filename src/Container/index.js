51d1fbb38499106e4c3f594aa6da7cfc35151afe
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-05 19:10:12 +0200

    Container component created with styled.js

---
+++ src/Container/index.js
@@ -0,0 +1,8 @@
+import { Wrapper } from "./styled";
+
+const Container = ({ children }) => (
+    <Wrapper>
+        {children}
+    </Wrapper>
+);
+export default Container;
---
+++ src/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 100%;
+    max-width: 1300px;
+    margin: auto;
+    background-color: red;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+`;
