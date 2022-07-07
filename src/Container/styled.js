b203340a3fb48254be909ace9fbdd29e4d6d82d1
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-07 13:40:32 +0200

    gap and witdh changed

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-    max-width: 1400px;
+    max-width: 1300px;
     width: 100%;
     margin: 180px auto;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-gap: 35px;
+    grid-gap: 100px;
 
     @media(max-width: 1350px){
        display: flex;
        flex-direction: column;
        align-items: center;
        margin-top: 20px;
+       grid-gap: 50px;
     }
 
     @media(max-width: 767px){
        width: 95%;
        margin: 20px 10px;
+       grid-gap: 30px;
     }
 `;
