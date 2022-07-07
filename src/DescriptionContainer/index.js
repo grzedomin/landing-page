d588b3e3ab31f0d6e19a1696d53e666a21cb65ca
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-07 10:48:35 +0200

    text content styled

--- src/DescriptionContainer/index.js
+++ src/DescriptionContainer/index.js
@@ -1,16 +1,15 @@
-
+import {Wrapper, Header, Text} from "./styled";
 
 export const DescriptionContainer = () => {
 
     return (
-        <div>
-
-            <h1>
-                Learn to code by watching others
-            </h1>
-            <p>
+        <Wrapper>
+            <Header>
+                Learn to code by <br/> watching others
+            </Header>
+            <Text>
                 See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.
-            </p>
-        </div>
+            </Text>
+        </Wrapper>
     );
 };
---
+++ src/DescriptionContainer/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+`;
+
+export const Header = styled.h1`
+    font-size: 55px;
+    font-weight: 700;
+`;
+
+export const Text = styled.p`
+    font-size: 20px;
+    margin: 0;
+`;
