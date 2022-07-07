add7095b2752f342ffefd24b2159e78130d1110b
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-07 10:46:41 +0200

    container display changed to grid

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -3,9 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     width: 100%;
     max-width: 1300px;
-    margin: auto;
-    background-color: red;
+    margin: 220px auto;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-gap: 10px;
+    grid-gap: 35px;
 `;
