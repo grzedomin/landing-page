14359d6bbdfc3bcff6c332caa9e5d4a216a9e205
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-19 22:38:21 +0200

    styled-normalize installed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from 'styled-normalize';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Normalize />
     <GlobalStyle />
     <App />
   </React.StrictMode>
